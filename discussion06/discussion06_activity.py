43c2e9da9d31acd1c281f049aa4624dcdf3ef5d8
llarppio <detectivelaura@gmail.com>
2025-10-16 12:52:36 -0700

    Update discussion06_activity.py

--- discussion06/discussion06_activity.py
+++ discussion06/discussion06_activity.py
@@ -10,13 +10,17 @@ if __name__ == '__main__':
 		num1 = int(input('Enter numerator >> '))
 		num2 = int(input('Enter denominator >> '))
 		### FIXME what happens if denom is 0?
+		if num2 = 0:
+			return print("Denominator cannot be 0")
 		print(num1, '/', num2, '=', num1/num2)
 		### FIXME write the inputs/outputs and any errors encountered
 		### to a log file
-
+		return num1 / num2
+		
 		### FIXME is this the best way to do this?
 		### Should we be checking inputs?
 		### What if something not y/n is entered?
 		rerun = input('Keep going? [Y/n] >> ')
 		if rerun not in ['Y', 'y']:
-			keep_going = False
+			return break
+
